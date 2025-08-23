529f42de2858fa28009f3e3110159e7435abafb8
PraveenKumarM17 <kanniymma@gmail.com>
2025-08-23 12:53:02 +0530

    invertedNumberRightPyramid.java file added.

---
+++ example.java/src/invertedNumberRightPyramid.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class invertedNumberRightPyramid {
+    public static void main (String[] args) {
+        Scanner sc =  new Scanner(System.in);
+
+        int n,m;
+
+        System.out.println("Enter the size of the tringle : ");
+        n = sc.nextInt();
+
+        System.out.println();
+
+
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= n; j++ ) {
+                if (j <= i) 
+                    System.out.print(j+" ");
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
