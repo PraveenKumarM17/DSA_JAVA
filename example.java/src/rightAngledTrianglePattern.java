1f491f0ff87d991231a9761982dbf9ab774d714e
PraveenKumarM17 <kanniymma@gmail.com>
2025-08-23 12:13:29 +0530

    rightAngledTrianglePattern.java added

--- example.java/src/rightAngledTrianglePattern.java
+++ example.java/src/rightAngledTrianglePattern.java
@@ -1,3 +1,27 @@
+import java.util.Scanner;
+
 public class rightAngledTrianglePattern {
-    
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n,m;
+
+        System.out.println("Enter the size (height & width) :");
+        n = sc.nextInt();
+        m = sc.nextInt();
+        
+        System.out.println();
+
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (j <= i)
+                System.out.print("* ");
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
 }
