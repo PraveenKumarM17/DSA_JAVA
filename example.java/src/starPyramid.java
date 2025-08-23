afda04fe7b37270255746723d9f71ac5f511d5cb
PraveenKumarM17 <kanniymma@gmail.com>
2025-08-23 14:52:33 +0530

    changed the variable initia value from 0 -> 1

--- example.java/src/starPyramid.java
+++ example.java/src/starPyramid.java
@@ -7,12 +7,12 @@ public class starPyramid {
         System.out.print("Enter the height : ");
         int n = sc.nextInt();
 
-        for (int i = 0; i <= n; i++) {
-            for (int j = 0; j < n-i; j++) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j < n-i+1; j++) {
                 System.out.print(" ");
             }
 
-            for (int j = 0; j < 2*i - 1; j++) {
+            for (int j = 1; j < 2*i; j++) {
                 System.out.print("*");
             }
 
