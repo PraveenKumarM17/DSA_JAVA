d0f67b0f3fa50c0845d50f181e0acbccbfb58dce
PraveenKumarM17 <kanniymma@gmail.com>
2025-08-23 15:14:49 +0530

    invertedStarPyramid.java file added.

---
+++ example.java/src/invertedStarPyramid.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class invertedStarPyramid {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the height : ");
+        int n = sc.nextInt();
+
+        for (int i = n; i > 0; i--) {
+            for (int j = 1; j <= n-i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= 2*i-1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        sc.close();
+
+    }
+} 
