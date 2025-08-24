c9859a877efc9e91af5c2bba7c677aa935e78e40
PraveenKumarM17 <kanniymma@gmail.com>
2025-08-24 21:19:00 +0530

    Added the better and Optimal solution to the GCD.

--- example.java/src/DemoFile.java
+++ example.java/src/DemoFile.java
@@ -3,14 +3,13 @@ import java.util.*;
 public class DemoFile {
 
     public static int findGCD(int n1, int n2) {
-        int gcd = 1;
 
-        for (int i = 1; i <= Math.min(n1, n2); i++) {
+        for (int i = Math.min(n1, n2); i > 0 ; i--) {
             if (n1 % i == 0 && n2 % i == 0) {
-                gcd = i;
+                return i;
             }
         }
-        return gcd;
+        return 0;
     }
     public static void main (String[] args) {
         Scanner sc = new Scanner(System.in);
