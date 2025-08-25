7ee86038a095662c8442aa65b5db8087e15ea2cb
PraveenKumarM17 <kanniymma@gmail.com>
2025-08-25 23:03:46 +0530

    alphaHillPattern.java file had some logical error has been resolved.

--- example.java/Basics/alphaHillPattern.java
+++ example.java/Basics/alphaHillPattern.java
@@ -17,8 +17,10 @@ public class alphaHillPattern {
                 System.out.print(ch+++" ");
             }
 
+            ch--;
+
             for (int j = 1; j < i; j++) {
-                System.out.print(ch--+" ");
+                System.out.print(--ch+" ");
             }
             System.out.println();
         }
