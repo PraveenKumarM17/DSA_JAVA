775d12a660f539b33c223fd77925379dcf9a19a7
PraveenKumarM17 <kanniymma@gmail.com>
2025-08-25 22:46:39 +0530

    alphaHillPattern.java file has been added to the basics folder.

---
+++ example.java/Basics/alphaHillPattern.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class alphaHillPattern {
+     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the Number: ");
+        int n = sc.nextInt();
+
+        
+        for (int i = 1; i <= n; i++) {
+            char ch = 'A';
+            for (int j = 1; j <= n-i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print(ch+++" ");
+            }
+
+            for (int j = 1; j < i; j++) {
+                System.out.print(ch--+" ");
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
